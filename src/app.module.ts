c300b862be64d1a96006d92884b34ffb31f1e814
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-09-21 18:19:41 -0300

    feat: ajustando cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ComputersModule } from './computers/computers.module';
 
 @Module({
@@ -28,7 +26,5 @@ import { ComputersModule } from './computers/computers.module';
     AuthModule,
     ComputersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://10.0.0.39:3000'], // coloque aqui a URL do seu frontend
+    origin: [
+      'http://localhost:3000',
+      'http://10.0.0.39:3000',
+      'https://gerenciamento-lw-frontend.vercel.app',
+    ], // coloque aqui a URL do seu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // se usar cookies
   });
