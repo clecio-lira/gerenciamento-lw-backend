d9b0c4cdba41495fb87abf47c38181d35d6e844f
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-09-21 12:52:19 -0300

    feat: ajustando cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://10.0.0.39:3000'], // coloque aqui a URL do seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // se usar cookies
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
