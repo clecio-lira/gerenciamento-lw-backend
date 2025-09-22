14b33a99d4a40ea9b904eeeb2deb993cd74ea2d0
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-09-22 08:23:56 -0300

    feat: cors ok

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://10.0.0.39:3000',
-      'http://172.20.0.106:3000/',
+      'http://172.20.0.106:3000',
       'https://gerenciamento-lw-frontend.vercel.app',
     ], // coloque aqui a URL do seu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
