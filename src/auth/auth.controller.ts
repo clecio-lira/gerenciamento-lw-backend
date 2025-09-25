0b5ebd676dcfa845f3b7b9dac353709f5c3f94f3
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-09-25 09:52:33 -0300

    feat: comentando rota register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,12 +13,12 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
-  register(
-    @Body() body: { username: string; email: string; password: string },
-  ) {
-    return this.authService.register(body.username, body.email, body.password);
-  }
+  // @Post('register')
+  // register(
+  //   @Body() body: { username: string; email: string; password: string },
+  // ) {
+  //   return this.authService.register(body.username, body.email, body.password);
+  // }
 
   @Post('login')
   login(@Body() body: { email: string; password: string }) {
