7bdf7d2453f7e655bb182ad2fab037538f0f87be
Clécio de Lira Alves <cclecioc.com@gmail.com>
2025-10-01 09:14:26 -0300

    feat: adicionando novo banco de dados

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     credentials: true, // se usar cookies
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
